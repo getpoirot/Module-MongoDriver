8c4f79663aa4a4e4c2aab11a1d6e8fd0b9387cc6
Payam Naderi <naderi.payam@gmail.com>
2016-06-29 15:10:59 +0430

    implement abstracts

---
+++ src/MongoDriver/Model/Repository/aRepository.php
@@ -0,0 +1,120 @@
+<?php
+namespace Module\MongoDriver\Model\Repository;
+
+use \MongoDB;
+
+class aRepository
+{
+    /** @var MongoDB\Database */
+    protected $gateway;
+    /** @var string Collection Name That Query Executed On */
+    protected $collection_name;
+    /** @var MongoDB\BSON\Persistable Data Object Model*/
+    protected $persist;
+    
+    /** @var MongoDB\Database Prepared DB Gateway With Options */
+    protected $_q;
+    
+
+    /**
+     * RepositoryCategories constructor.
+     *
+     * @param MongoDB\Database $mongoDb
+     * @param string           $collection
+     */
+    function __construct(MongoDB\Database $mongoDb, $collection, MongoDB\BSON\Persistable $persistable = null)
+    {
+        $this->setGateway($mongoDb);
+        $this->setDbCollection($collection);
+        
+        if ($persistable) 
+            $this->setModelPersist($persistable);
+        
+        $this->__init();
+    }
+
+    /**
+     * Initialize Object 
+     * 
+     */
+    protected function __init()
+    {
+        // Implement Construct Initialization
+    }
+    
+    
+    // Options:
+
+    /**
+     * Set Data Gateway
+     *
+     * @param MongoDB\Database $mongoClient
+     *
+     * @return $this
+     */
+    function setGateway(MongoDB\Database $mongoClient)
+    {
+        // reset _query collection
+        $this->_q = null;
+
+        $this->gateway = $mongoClient;
+        return $this;
+    }
+
+    /**
+     * Set Db Categories Collection Name
+     *
+     * @param string $collectionName
+     *
+     * @return $this
+     */
+    function setDbCollection($collectionName)
+    {
+        // reset _query collection
+        $this->_q = null;
+
+        $this->collection_name = $collectionName;
+        return $this;
+    }
+
+    /**
+     * Set Data Model Object
+     * 
+     * @param MongoDB\BSON\Persistable $persistable
+     * 
+     * @return $this
+     */
+    function setModelPersist(MongoDB\BSON\Persistable $persistable)
+    {
+        // reset _query collection
+        $this->_q = null;
+        
+        $this->persist = get_class($persistable);
+        return $this;
+    }
+    
+    
+    // ..
+
+    /**
+     * prepared mongo client with options
+     * - select db collection
+     * 
+     * @return MongoDB\Collection
+     */
+    protected function _query()
+    {
+        if ($this->_q)
+            return $this->_q;
+
+        $db = $this->gateway;
+        if ($this->persist) {
+            $db = $db->withOptions(array('typeMap' => array(
+                'root'     => $this->persist,
+            )));
+        }
+
+        $this->_q = $db;
+        return $this->_q->selectCollection($this->collection_name);
+    }
+}
---
+++ src/MongoDriver/Model/aPersistable.php
@@ -0,0 +1,46 @@
+<?php
+namespace Module\MongoDriver\Model;
+
+use MongoDB\BSON\Persistable;
+use MongoDB\BSON\Serializable;
+use MongoDB\BSON\Unserializable;
+
+use Poirot\Std\Struct\DataMean;
+
+class aPersistable extends DataMean
+    implements Serializable
+    , Unserializable
+    , Persistable
+{
+    protected $properties = array (
+        // '_id'      => null,
+    );
+    
+    
+    // Implement Serializable / Unserializable
+
+    /**
+     * Provides an array or document to serialize as BSON
+     * Called during serialization of the object to BSON. The method must return an array or stdClass.
+     * Root documents (e.g. a MongoDB\BSON\Serializable passed to MongoDB\BSON\fromPHP()) will always be serialized as a BSON document.
+     * For field values, associative arrays and stdClass instances will be serialized as a BSON document and sequential arrays (i.e. sequential, numeric indexes starting at 0) will be serialized as a BSON array.
+     * @link http://php.net/manual/en/mongodb-bson-serializable.bsonserialize.php
+     * @return array|object An array or stdClass to be serialized as a BSON array or document.
+     */
+    function bsonSerialize()
+    {
+        return $this->properties;
+    }
+
+    /**
+     * Constructs the object from a BSON array or document
+     * Called during unserialization of the object from BSON.
+     * The properties of the BSON array or document will be passed to the method as an array.
+     * @link http://php.net/manual/en/mongodb-bson-unserializable.bsonunserialize.php
+     * @param array $data Properties within the BSON array or document.
+     */
+    function bsonUnserialize(array $data)
+    {
+        $this->import($data);
+    }
+}

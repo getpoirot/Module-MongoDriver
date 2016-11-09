fe20afe2557039071a06a5ee56f1e160c7188790
Payam Naderi <naderi.payam@gmail.com>
2016-11-09 10:22:49 +0330

    implement config load by poirot loader

--- src/MongoDriver/Module.php
+++ src/MongoDriver/Module.php
@@ -80,7 +80,7 @@ class Module implements iSapiModule
      */
     function initConfig(iDataEntity $config)
     {
-        return include __DIR__.'/../../config/module.conf.php';
+        return \Poirot\Config\load(__DIR__ . '/../../config/mod-driver_mongo');
     }
 
     /**

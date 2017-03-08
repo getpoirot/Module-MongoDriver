2529069079155821b592665bdd8df97a25873801
Payam Naderi <naderi.payam@gmail.com>
2017-03-08 22:52:33 +0330

    implement ObjectID Interface

---
+++ src/MongoDriver/Interfaces/iObjectID.php
@@ -0,0 +1,14 @@
+<?php
+namespace Module\MongoDriver\Interfaces;
+
+interface iObjectID
+{
+    /**
+     * iObjectID constructor.
+     * 
+     * @param null|string $id
+     */
+    function __construct($id = null);
+    
+    function __toString();
+}

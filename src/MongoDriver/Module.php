5de74a37de15036080f19d43c4df63c68100aba7
Payam Naderi <naderi.payam@gmail.com>
2017-05-17 13:55:48 +0430

    init module action aware

--- src/MongoDriver/Module.php
+++ src/MongoDriver/Module.php
@@ -1,98 +1,110 @@
 <?php
-namespace Module\MongoDriver;
+namespace Module\MongoDriver
+{
+    use Poirot\Application\aSapi;
+    use Poirot\Application\Interfaces\iApplication;
+    use Poirot\Application\Interfaces\Sapi;
+    use Poirot\Application\Interfaces\Sapi\iSapiModule;
+    use Poirot\Application\Sapi\Module\ContainerForFeatureActions;
 
-use Poirot\Application\aSapi;
-use Poirot\Application\Interfaces\iApplication;
-use Poirot\Application\Interfaces\Sapi;
-use Poirot\Application\Interfaces\Sapi\iSapiModule;
-use Poirot\Application\Sapi\Module\ContainerForFeatureActions;
+    use Poirot\Ioc\Container\BuildContainer;
+    use Poirot\Loader\Autoloader\LoaderAutoloadAggregate;
+    use Poirot\Loader\Autoloader\LoaderAutoloadNamespace;
+    use Poirot\Loader\Interfaces\iLoaderAutoload;
 
-use Poirot\Ioc\Container\BuildContainer;
-use Poirot\Loader\Autoloader\LoaderAutoloadAggregate;
-use Poirot\Loader\Autoloader\LoaderAutoloadNamespace;
-use Poirot\Loader\Interfaces\iLoaderAutoload;
-
-use Poirot\Std\Interfaces\Struct\iDataEntity;
+    use Poirot\Std\Interfaces\Struct\iDataEntity;
 
 // TODO Implement Model Sample Structure to local or something default collection db
 
-class Module implements iSapiModule
-    , Sapi\Module\Feature\iFeatureModuleInitSapi
-    , Sapi\Module\Feature\iFeatureModuleAutoload
-    , Sapi\Module\Feature\iFeatureModuleMergeConfig
-    , Sapi\Module\Feature\iFeatureModuleNestActions
+    class Module implements iSapiModule
+        , Sapi\Module\Feature\iFeatureModuleInitSapi
+        , Sapi\Module\Feature\iFeatureModuleAutoload
+        , Sapi\Module\Feature\iFeatureModuleMergeConfig
+        , Sapi\Module\Feature\iFeatureModuleNestActions
+    {
+        const CONF_KEY = 'module.mongo_driver';
+
+        /**
+         * Init Module Against Application
+         *
+         * - determine sapi server, cli or http
+         *
+         * priority: 1000 A
+         *
+         * @param iApplication|aSapi $sapi Application Instance
+         *
+         * @return false|null False mean not setup with other module features (skip module)
+         */
+        function initialize($sapi)
+        {
+            if (! extension_loaded('mongodb') )
+                throw new \RuntimeException('Mongodb driver extension not installed.');
+
+
+        }
+
+        /**
+         * Register class autoload on Autoload
+         *
+         * priority: 1000 B
+         *
+         * @param LoaderAutoloadAggregate $baseAutoloader
+         *
+         * @return iLoaderAutoload|array|\Traversable|void
+         */
+        function initAutoload(LoaderAutoloadAggregate $baseAutoloader)
+        {
+            #$nameSpaceLoader = \Poirot\Loader\Autoloader\LoaderAutoloadNamespace::class;
+            $nameSpaceLoader = 'Poirot\Loader\Autoloader\LoaderAutoloadNamespace';
+            /** @var LoaderAutoloadNamespace $nameSpaceLoader */
+            $nameSpaceLoader = $baseAutoloader->loader($nameSpaceLoader);
+            $nameSpaceLoader->addResource(__NAMESPACE__, __DIR__);
+
+
+            require_once __DIR__.'/_functions.php';
+            require_once __DIR__.'/_ioc-facade.php';
+        }
+
+        /**
+         * Register config key/value
+         *
+         * priority: 1000 D
+         *
+         * - you may return an array or Traversable
+         *   that would be merge with config current data
+         *
+         * @param iDataEntity $config
+         *
+         * @return array|\Traversable
+         */
+        function initConfig(iDataEntity $config)
+        {
+            return \Poirot\Config\load(__DIR__ . '/../../config/mod-driver_mongo');
+        }
+
+        /**
+         * Get Action Services
+         *
+         * priority: after GrabRegisteredServices
+         *
+         * - return Array used to Build ModuleActionsContainer
+         *
+         * @return array|ContainerForFeatureActions|BuildContainer|\Traversable
+         */
+        function getActions()
+        {
+            return \Poirot\Config\load(__DIR__ . '/../../config/mod-driver_mongo.actions');
+        }
+    }
+}
+
+
+namespace Module\MongoDriver\Actions
 {
-    const CONF_KEY = 'module.mongo_driver';
 
     /**
-     * Init Module Against Application
-     *
-     * - determine sapi server, cli or http
-     *
-     * priority: 1000 A
-     *
-     * @param iApplication|aSapi $sapi Application Instance
-     *
-     * @return false|null False mean not setup with other module features (skip module)
+     * @method static MongoDriverAction Driver()
      */
-    function initialize($sapi)
-    {
-        if (! extension_loaded('mongodb') )
-            throw new \RuntimeException('Mongodb driver extension not installed.');
-        
-        
-    }
-
-    /**
-     * Register class autoload on Autoload
-     *
-     * priority: 1000 B
-     *
-     * @param LoaderAutoloadAggregate $baseAutoloader
-     *
-     * @return iLoaderAutoload|array|\Traversable|void
-     */
-    function initAutoload(LoaderAutoloadAggregate $baseAutoloader)
-    {
-        #$nameSpaceLoader = \Poirot\Loader\Autoloader\LoaderAutoloadNamespace::class;
-        $nameSpaceLoader = 'Poirot\Loader\Autoloader\LoaderAutoloadNamespace';
-        /** @var LoaderAutoloadNamespace $nameSpaceLoader */
-        $nameSpaceLoader = $baseAutoloader->loader($nameSpaceLoader);
-        $nameSpaceLoader->addResource(__NAMESPACE__, __DIR__);
-
-
-        require_once __DIR__.'/_functions.php';
-        require_once __DIR__.'/_ioc-facade.php';
-    }
-    
-    /**
-     * Register config key/value
-     *
-     * priority: 1000 D
-     *
-     * - you may return an array or Traversable
-     *   that would be merge with config current data
-     *
-     * @param iDataEntity $config
-     *
-     * @return array|\Traversable
-     */
-    function initConfig(iDataEntity $config)
-    {
-        return \Poirot\Config\load(__DIR__ . '/../../config/mod-driver_mongo');
-    }
-
-    /**
-     * Get Action Services
-     *
-     * priority: after GrabRegisteredServices
-     *
-     * - return Array used to Build ModuleActionsContainer
-     *
-     * @return array|ContainerForFeatureActions|BuildContainer|\Traversable
-     */
-    function getActions()
-    {
-        return \Poirot\Config\load(__DIR__ . '/../../config/mod-driver_mongo.actions');
-    }
+    class IOC extends \IOC
+    { }
 }
